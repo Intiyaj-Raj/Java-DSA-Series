44e1cff926b3cbd13a334716f491f960b8abb444
Intiyaj-Raj <intiyajansari8757@gmail.com>
2025-10-22 03:33:04 +0530

    alphabet pattern

--- src/loopProblem/alphabetPattern/PatternA.java
+++ src/loopProblem/alphabetPattern/PatternA.java
@@ -25,7 +25,8 @@ public class PatternA {
             System.out.println(" ");
         }
 
-
+        System.out.println();
+        System.out.println("--------------------------------------------------");
 
         /*
         *
@@ -37,11 +38,23 @@ public class PatternA {
 
 
         * */
+        // for rows 1 to 5
         for (int i=1;i<=5;i++)
         {
+            // for cols 1 to 9
             for (int j=1;j<=9;j++)
             {
-                if(j==6-i || i==3 && j>=3 && j<=7 || j==4+i){
+                /*
+                        j == 6 - i
+                        Draws a left diagonal line
+
+                        j == 4 + i
+                        Draws a right diagonal line
+
+                        i == 3 && j >= 3 && j <= 7
+                        When i = 3, it draws a horizontal line from column 3 to 7.
+                */
+                if (j == 6 - i || j == 4 + i || (i == 3 && j >= 3 && j <= 7)) {
                     System.out.print("+");
                 }
                 else {
