daf1d6dd8f1f1e8361fc9091180380274412046d
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-05 14:49:32 +0530

    find max of 3 num

---
+++ src/basics/max_3_number.java
@@ -0,0 +1,29 @@
+package basics;
+
+import java.util.Scanner;
+
+public class max_3_number {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num1: ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter num2: ");
+        int n2 = sc.nextInt();
+        System.out.print("Enter num3: ");
+        int n3 = sc.nextInt();
+
+        if (n1>n2 && n1>n3){
+            System.out.println(n1 + " is greater than " + n2+" and " + n3);
+        } else if (n2>n1 && n2>n3) {
+            System.out.println(n2 +" is greater than "+ n1+ " and "+ n3);
+        }
+        else if (n3>n1 && n3>n2){
+            System.out.println(n3 + " is greater than " + n1 +" and "+ n2);
+        }
+        else{
+            System.out.println("all numbers are equal");
+        }
+    }
+}
