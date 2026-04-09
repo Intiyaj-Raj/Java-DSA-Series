754b65632e4d7bfbae56a6415bd016d94bf30eb3
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-09 20:19:02 +0530

    check numbe\r is positive or not

---
+++ src/basics/checkPositiveNegative.java
@@ -0,0 +1,18 @@
+package basics;
+
+import java.util.Scanner;
+
+public class checkPositiveNegative {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n>=0){
+            System.out.println("Positive Number");
+        }
+        else{
+            System.out.println("Negative Number");
+        }
+    }
+}
