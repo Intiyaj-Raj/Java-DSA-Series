ac8da5a1bbef780a0b4a23a93446fe64f8e057a5
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-09 20:22:56 +0530

    check you have a fever or not

--- src/basics/checkPositiveNegative.java
+++ src/basics/checkPositiveNegative.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 
 public class checkPositiveNegative {
     public static void main(String[] args) {
-
+//question 1
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if (n>=0){
+        if (n >= 0) {
             System.out.println("Positive Number");
-        }
-        else{
+        } else {
             System.out.println("Negative Number");
+        }
+
+
+//        Question 2
+
+        double num = 120.5;
+
+        if (num > 100) {
+            System.out.println("You have a fever.");
+        } else {
+            System.out.println("You don't have a fever.");
         }
     }
 }
