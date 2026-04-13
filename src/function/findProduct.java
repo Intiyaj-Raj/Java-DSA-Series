0fc5305ffa0e9e55919f6f484108d63f3f73992f
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-13 17:54:28 +0530

    find product

---
+++ src/function/findProduct.java
@@ -0,0 +1,16 @@
+package function;
+
+public class findProduct {
+
+    static int calculateProduct(int n1,int n2){
+        return n1*n2;
+    }
+    public static void main(String[] args) {
+
+        int result = calculateProduct(12,10);
+
+        System.out.println("product of two num = " +result);
+
+    }
+
+}
