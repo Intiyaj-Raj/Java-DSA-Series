75d32251fede9b8d5bd3fb71e63933ec07ca7153
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-19 04:49:06 +0530

    solve alphabet sq pattern

--- src/loop_pattern_revision/square/AlphabetSquare.java
+++ src/loop_pattern_revision/square/AlphabetSquare.java
@@ -26,17 +26,24 @@ public class AlphabetSquare {
         System.out.println();
         System.out.println();
 
+
+                /*
+                    a a a a
+                    B B B B
+                    c c c c
+                    D D D D
+                */
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i % 2 == 0) {
-                    int a = i+97;
-                    System.out.print((char) (a) + " ");
-                    a+=2;
-                } else {
                     System.out.print((char) (i + 64) + " ");
+                } else {
+                    System.out.print((char) (i + 96) + " ");
                 }
             }
             System.out.println();
         }
+
     }
 }
