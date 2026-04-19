85c34aa2f26c662bc79bd4fb4c659fe957e4bc79
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-19 21:37:38 +0530

    solve odd even triangle pattern

---
+++ src/loop_pattern_revision/triangle/EvenTriangle.java
@@ -0,0 +1,17 @@
+package loop_pattern_revision.triangle;
+
+public class EvenTriangle {
+    public static void main(String[] args) {
+
+    int n = 5;
+    int a = 0;
+
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            System.out.print(a+" ");
+            a+=2;
+        }
+        System.out.println();
+    }
+    }
+}
