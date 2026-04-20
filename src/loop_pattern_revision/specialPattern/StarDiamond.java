b9dd228fe71401f99d8c077a7aa4de206c282358
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-21 03:38:48 +0530

    solve diamond  pattern problem

---
+++ src/loop_pattern_revision/specialPattern/StarDiamond.java
@@ -0,0 +1,35 @@
+package loop_pattern_revision.specialPattern;
+
+public class StarDiamond {
+    public static void main(String[] args) {
+
+        int n = 5;
+        int nsp = n-1;
+        int nst = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("* ");
+            }
+            nsp--;
+            nst += 2;
+            System.out.println();
+        }
+
+        nsp = 1;
+        nst = 2*n-3;
+        for (int i=1;i<=n-1;i++){
+            for (int j=1;j<=nsp;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=nst;j++){
+                System.out.print("* ");
+            }
+            nsp++;
+            nst-=2;
+            System.out.println();
+        }
+    }
+}
