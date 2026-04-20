0e55cb097e8bac5ef3178c7fb7c337206afd7158
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-20 22:56:17 +0530

    solve flipped vertical pattern

---
+++ src/loop_pattern_revision/specialPattern/TriangleFlippedPattern.java
@@ -0,0 +1,23 @@
+package loop_pattern_revision.specialPattern;
+
+public class TriangleFlippedPattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i=1;i<=n;i++){
+            // print spaces
+            for (int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+
+            // print stars
+            for (int j =1;j<=i;j++){
+                System.out.print("* ");
+            }
+
+            // new line
+            System.out.println();
+        }
+    }
+}
