7486161c10be7af29aa014abbf3965878f9d6370
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-20 23:13:05 +0530

    solve rhombus pattern

--- src/loop_pattern_revision/specialPattern/Rhombus.java
+++ src/loop_pattern_revision/specialPattern/Rhombus.java
@@ -11,12 +11,8 @@ public class Rhombus {
                 System.out.print("  ");
             }
 
-
             for (int j = 1; j <= n; j++) {
                 System.out.print("* ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("  ");
             }
 
             System.out.println();
